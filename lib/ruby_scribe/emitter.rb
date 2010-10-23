fa454f2a0dac154a8180989be6a3c61c1bed1580
Ben Hughes <ben@railsgarden.com>
2010-10-23 22:53:53 +0300

    Further enhancements and implementation through testing.

--- lib/ruby_scribe/emitter.rb
+++ lib/ruby_scribe/emitter.rb
@@ -9,9 +9,13 @@ module RubyScribe
     end
     
     def emit(sexp)
+      return unless sexp
+      
       case sexp.sexp_type
       when :block
         emit_block(sexp)
+      when :rescue
+        emit_rescue(sexp)
       when :class
         emit_class_definition(sexp)
       when :module
@@ -20,19 +24,28 @@ module RubyScribe
         emit_method_definition(sexp)
       when :call
         emit_method_call(sexp)
+      when :attrasgn
+        emit_attribute_assignment(sexp)
       when :if, :unless
         emit_conditional_block(sexp)
+      when :case
+        emit_case_statement(sexp)
       when :while, :until
         emit_loop_block(sexp)
-      when :lasgn
+      when :lasgn, :iasgn
         emit_assignment_expression(sexp)
+      when :op_asgn1
+        emit_optional_assignment_expression(sexp)
       when :iter
         emit_block_invocation(sexp)
-      when :str, :lit, :true, :false
+      when :defined
+        emit_defined_invocation(sexp)
+      when :str, :lit, :lvar, :ivar, :const, :true, :false, :colon2, :hash
         emit_token(sexp)
       else
         emit_unknown_expression(sexp)
       end
+      @output
     end
     
     
@@ -44,6 +57,23 @@ module RubyScribe
         emit(child)
         eline
       end
+    end
+    
+    def emit_rescue(sexp)
+      eline "begin"
+
+      indent do
+        emit(sexp.sexp_body[0])
+      end
+      
+      resbody = sexp.sexp_body[1].sexp_body
+      line "rescue #{resbody[0].inspect}"
+      
+      indent do
+        emit(resbody[1])
+      end
+      
+      line "end"
     end
     
     def emit_class_definition(sexp)
@@ -58,6 +88,7 @@ module RubyScribe
     end
     
     def emit_module_definition(sexp)
+      line
       line "module #{sexp.sexp_body[0]}"
       
       indent do
@@ -65,7 +96,6 @@ module RubyScribe
       end
       
       line "end"
-      line
     end
     
     def emit_method_definition(sexp)
@@ -89,12 +119,20 @@ module RubyScribe
       
       if sexp.sexp_body[2].sexp_body[0]
         segment "("
-        emit_method_argument_list(sexp.sexp_body[2])
+        emit_argument_list(sexp.sexp_body[2])
         segment ")"
       end
     end
     
-    def emit_method_argument_list(sexp)
+    def emit_attribute_assignment(sexp)
+      segment sexp.sexp_body[0][0].to_s
+      segment "."
+      segment sexp.sexp_body[1].to_s.gsub(/=$/, "")
+      segment " = "
+      emit_argument_list(sexp.sexp_body[2])
+    end
+    
+    def emit_argument_list(sexp)
       sexp.sexp_body.each_with_index do |arg, i|
         segment ", " unless i == 0
         emit(arg)
@@ -121,7 +159,25 @@ module RubyScribe
         end
       end
       
-      line "end"
+      sline "end"
+    end
+    
+    def emit_case_statement(sexp)
+      segment "case "
+      emit sexp.sexp_body[0]
+      eline
+      sexp.sexp_body[1..-1].each do |child|
+        emit_case_when_statement child
+      end
+    end
+    
+    def emit_case_when_statement(sexp)
+      return unless sexp
+      
+      sline "when "
+      emit sexp.sexp_body[0]
+      eline
+      emit sexp.sexp_body[1]
     end
     
     def emit_loop_block(sexp)
@@ -141,6 +197,16 @@ module RubyScribe
     def emit_assignment_expression(sexp)
       segment "#{sexp.sexp_body[0]} = "
       emit sexp.sexp_body[1]
+    end
+    
+    def emit_optional_assignment_expression(sexp)
+      emit sexp.sexp_body[0]
+      segment "["
+      emit_argument_list sexp.sexp_body[1]
+      segment "] "
+      segment sexp.sexp_body[2].to_s
+      segment "= "
+      emit sexp.sexp_body[3]
     end
     
     def emit_block_invocation(sexp)
@@ -163,18 +229,40 @@ module RubyScribe
       end
     end
     
+    def emit_defined_invocation(sexp)
+      segment "defined?(:"
+      emit sexp.sexp_body[0]
+      segment ")"
+    end
+    
     def emit_token(sexp)
       case sexp.sexp_type
       when :str
         segment '"' + sexp.sexp_body[0] + '"'
       when :lit
-        segment sexp.sexp_body[0].inspect
+        segment ":" + sexp.sexp_body[0].to_s
+      when :const
+        segment sexp.sexp_body[0].to_s
+      when :lvar
+        segment sexp.sexp_body[0].to_s
+      when :ivar
+        segment sexp.sexp_body[0].to_s
       when :true
         segment "true"
       when :false
         segment "false"
+      when :colon2
+        emit sexp.sexp_body[0]
+        segment "::"
+        segment sexp.sexp_body[1].to_s
+      when :hash
+        sexp.sexp_body.in_groups_of(2) do |group|
+          emit(group[0])
+          segment " => "
+          emit(group[1])
+        end
       else
-        segment sexp.sexp_body
+        segment sexp.sexp_body.to_s
       end
     end
     
--- spec/examples/simple_class_with_methods.rb
+++ spec/examples/simple_class_with_methods.rb
@@ -1,12 +1,39 @@
-module JazzModel
-  class Base
-    attr_accessor :test
+module MicroSessions
+  class MicroSession
+    def initialize(controller)
+      @controller = controller
+    end
     
-    def test
-      if one
-        1
-      else
-        2
+    def options
+      @controller.class.micro_session_options
+    end
+    
+    def data
+      @controller.session[options[:key]] ||= {}
+      @controller.session[options[:key]][id] ||= {}
+    end
+    
+    delegate :empty?, :[], :[]=, :to_h, :to_hash, :to => :data, :another => :test
+    
+    def id
+      @id ||= id_from_params || generate_id
+    end
+    
+    
+    protected
+    
+    def id_from_params
+      @controller.params[options[:param]] if @controller.params
+    end
+    
+    def generate_id
+      case options[:param_type]
+      when :hash
+        ActiveSupport::SecureRandom.hex(options[:length] / 2)
+      when :integer
+        self.counter += 1
+      when :random_integer
+        ActiveSupport::SecureRandom.integer
       end
     end
   end
