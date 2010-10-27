1d9ce6caa641e8f2f29ad09e19d63c7d57f01d2b
Ben Hughes <ben@railsgarden.com>
2010-10-27 22:52:51 +0300

    Minor formatting changes to eachifier.

--- lib/ruby_scribe/transformers/eachifier.rb
+++ lib/ruby_scribe/transformers/eachifier.rb
@@ -2,23 +2,19 @@ module RubyScribe
   module Transformers
     class Eachifier < Transformer
       def transform(e)
-        return super unless e.is_a?(Sexp)
-        
-        if e.kind == :for
+        if e.is_a?(Sexp) && e.kind == :for
           transform_for_to_each(e)
         else
           super
         end
       end
       
-      
-      protected
-      
       def transform_for_to_each(e)
         s(:iter, 
           s(:call, e.body[0], :each, s(:arglist)), 
           e.body[1],
-          e.body[2])
+          e.body[2]
+        )
       end
     end
   end
